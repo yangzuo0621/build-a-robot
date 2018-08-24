452394b5b51d873896fe10b08a36e097ea0d4bac
Zuo Yang <zuya@microsoft.com>
2018-08-24 19:46:10 +0800

    passing data to parent component and inject content into a child component with slots

--- src/builder/PartSelector.vue
+++ src/builder/PartSelector.vue
@@ -19,7 +19,16 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  props: ['parts', 'position'],
+  props: {
+    parts: { type: Array, required: true },
+    position: {
+      type: String,
+      required: true,
+      validator: function(value) {
+        return ['left', 'right', 'top', 'bottom', 'center'].includes(value);
+      }
+    }
+  },
   data() {
     return { selectedPartIndex: 0 };
   },
@@ -28,18 +37,29 @@ export default {
       return this.parts[this.selectedPartIndex];
     }
   },
+  created() {
+    this.emitSelectedPart();
+  },
+  updated() {
+    this.emitSelectedPart();
+  },
   methods: {
+    emitSelectedPart() {
+      this.$emit('partSelected', this.selectedPart);
+    },
     selectNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
         this.parts.length
       );
+      // this.emitSelectedPart();
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length
       );
+      // this.emitSelectedPart();
     }
   }
 };
--- src/builder/RobotBuilder.vue
+++ src/builder/RobotBuilder.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    <div class="preview">
+      <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src"/>
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+            <img :src="selectedRobot.torso.src"/>
+            <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+          </div>
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src"/>
+          </div>
+        </div>
+      </CollapsibleSection>
+      <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>
     <div class="top-row">
       <!-- <div class="top part" :style="headBorderStyle"> -->
       <!-- <div class="top part" :class="{'sale-border': selectedRobot.head.onSale}"> -->
@@ -8,15 +25,15 @@
           {{selectedRobot.head.title}}
           <span v-show="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div> -->
-        <PartSelector :parts="availableParts.heads" position="top" />
+        <PartSelector :parts="availableParts.heads" position="top" @partSelected="part => selectedRobot.head=part" />
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms" position="left" />
-      <PartSelector :parts="availableParts.torsos" position="center" />
-      <PartSelector :parts="availableParts.arms" position="right" />
+      <PartSelector :parts="availableParts.arms" position="left" @partSelected="part => selectedRobot.leftArm=part" />
+      <PartSelector :parts="availableParts.torsos" position="center" @partSelected="part => selectedRobot.torso=part" />
+      <PartSelector :parts="availableParts.arms" position="right" @partSelected="part => selectedRobot.rightArm=part" />
     </div>
     <div class="bottom-row">
-      <PartSelector :parts="availableParts.bases" position="bottom" />
+      <PartSelector :parts="availableParts.bases" position="bottom" @partSelected="part => selectedRobot.base=part" />
     </div>
     <div>
       <h1>Cart</h1>
@@ -42,10 +59,11 @@
 import availableParts from '../data/parts';
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
+import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
-  components: { PartSelector },
+  components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
@@ -193,8 +211,7 @@ export default {
 }
 .add-to-cart {
   position: absolute;
-  right: 30px;
-  width: 220px;
+  width: 210px;
   padding: 3px;
   font-size: 16px;
 }
@@ -209,5 +226,26 @@ th {
 }
 .sale-border {
   border: 3px solid red;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
