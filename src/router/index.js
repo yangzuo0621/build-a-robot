1aceb6e6654a632815641140d47788997746d932
Zuo Yang <zuya@microsoft.com>
2018-08-29 09:59:15 +0800

    using nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../builder/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
 
 Vue.use(Router);
 
@@ -18,6 +23,17 @@ export default new Router({
       path: '/build',
       name: 'Build',
       component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        { path: 'heads', name: 'RobotHeads', component: RobotHeads },
+        { path: 'arms', name: 'RobotArms', component: RobotArms },
+        { path: 'toros', name: 'RobotTorsos', component: RobotTorsos },
+        { path: 'bases', name: 'RobotBases', component: RobotBases },
+      ],
     },
     {
       path: '/parts/:partType/:id',
